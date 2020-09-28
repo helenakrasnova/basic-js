a92b297aba3291ae6a60c51cc7221d0facba5f67
Helena Krasnova <alenka5354@mail.ru>
2020-09-29 00:51:17 +0300

    feat: add carbon and transform array tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof (sampleActivity) != 'string') {
+    return false;
+  }
+  let parsedString = parseFloat(sampleActivity);
+  if (isNaN(parsedString)) {
+    return false;
+  }
+  if (parsedString > MODERN_ACTIVITY || parsedString <= 0) {
+    return false;
+  }
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  let ln = Math.log10(MODERN_ACTIVITY / parsedString);
+  return Math.floor(ln / k);
+
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,47 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error();
+  }
+  let newArr = arrayCopy(arr);
+  const discardNext = '--discard-next';
+  const discardPrev = '--discard-prev';
+  const doubleNext = '--double-next';
+  const doublePrev = '--double-prev';
+
+  for (let i = 0; i < newArr.length; i++) {
+    if (newArr[i] === discardPrev) {
+      newArr[i - 1] = null;
+      newArr[i] = null;
+    }
+    else if (newArr[i] === discardNext) {
+      newArr[i + 1] = null;
+      newArr[i] = null;
+    }
+    else if (newArr[i] === doublePrev) {
+      newArr[i] = newArr[i - 1];
+    }
+    else if (newArr[i] === doubleNext) {
+      newArr[i] = newArr[i + 1];
+    }
+  }
+  let finalArr = newArr.filter((item) => item !== null);
+  return finalArr;
+}
+
+function arrayCopy(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if ((arr[i] === '--double-prev' || arr[i] === '--discard-prev') && i === 0) {
+      continue;
+    }
+    else if ((arr[i] === '--discard-next' || arr[i] === '--double-next') && i === arr.length - 1) {
+      continue;
+    }
+    else {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
