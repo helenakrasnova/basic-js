8887c0a9ed975db14f5669d617841bc3eb1a57b3
Helena Krasnova <alenka5354@mail.ru>
2020-10-01 20:07:50 +0300

    feat: add extended repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  str = '' + str;
+  options.addition = options.addition !== undefined ? '' + options.addition : '';
+  let additionArr = [];
+  let newArray = [];
+  options.additionRepeatTimes = options.additionRepeatTimes ? options.additionRepeatTimes : 1;
+  options.repeatTimes = options.repeatTimes ? options.repeatTimes : 1;
+  options.additionSeparator = options.additionSeparator ? options.additionSeparator : '';
+  options.separator = options.separator ? options.separator : '+';
+  options.addition = options.addition ? options.addition : '';
+  for (let i = 0; i < options.additionRepeatTimes; i++) {
+    additionArr.push(options.addition);
+  }
+
+  let addition = additionArr.join(options.additionSeparator);
+  let result = '' + str + addition;
+
+
+  for (let i = 0; i < options.repeatTimes; i++) {
+    newArray.push(result);
+  }
+
+  return newArray.join(options.separator);
 };
-  
+
+
+
+'la', { repeatTimes: 3, separator: 's' }, 'laslasla'
